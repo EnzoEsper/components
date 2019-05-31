613ec3efbee2fc4eee4524ca530d30b737e07455
EnzoEsper <enzoesper@hotmail.com>
2019-05-31 19:20:22 -0300

    vuelvo a romper

--- src/ApprovalCard.js
+++ src/ApprovalCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ApprovalCard = (props) => {
   return (
-    <div className="ui card" data-test="approvalCard">
+    <div className="ui card" data-test="approvalCardComp">
       <div className="content">{props.children}</div>
       <div className="extra content">
         <div className="ui two buttons" data-test="twoButtons">
